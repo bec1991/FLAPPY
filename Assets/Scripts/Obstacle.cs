375a7b82dff3a41ac16023a2c57f40392973ebf2
Kyle <silverwolf115432@yahoo.com.au>
2021-07-05 14:55:10 +1000

    Spawner and Game mechanics details

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     public float speed;
+    //public GameObject spawner;
+
 
     // Update is called once per frame
+    void Start()
+    {
+        speed = transform.parent.gameObject.GetComponent<Spawner>().obsSpeed;
+    }
+
+
+
     void Update()
     {
+
+
+
         transform.position += ((Vector3.left * speed) * Time.deltaTime);
+
     }
+
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,22 +7,45 @@ public class Spawner : MonoBehaviour
     public float queueTime = 1.5f;
     private float time = 0;
     public GameObject obstacle;
+    public float currentSpeed = 5f;
+    public float obsSpeed = 5f;
+    public float speedInc = 0.1f;
 
-    public float height;
+    public int height = 3;
 
     // Update is called once per frame
     void Update()
     {
+       StartCoroutine(DifficultySpeed());
+        WoodObstacle();
+
+        time += Time.deltaTime;
+        Debug.Log(time);
+
+    }
+
+    public void WoodObstacle()
+    {
         if (time > queueTime)
         {
-            GameObject go = Instantiate(obstacle);
+            GameObject go = Instantiate(obstacle, transform.position, Quaternion.identity, transform);
+            //obstacle.transform.parent = go.transform;
             go.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
 
             time = 0;
 
             Destroy(go, 10);
         }
-
-        time += Time.deltaTime;
     }
+
+    IEnumerator DifficultySpeed()
+    {
+        if (time > queueTime)
+        {
+            obsSpeed += speedInc;
+           yield return new WaitForSeconds(10);
+        }
+    }
+
+   
 }
